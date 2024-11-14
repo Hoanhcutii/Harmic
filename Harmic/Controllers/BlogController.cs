e6e46ac4ec4dad01850cdfb65b1509af11d1959d
Hoanhcutii <hoaianh03082k4@gmail.com>
2024-11-14 16:20:08 +0700

    init project

---
+++ Harmic/Controllers/BlogController.cs
@@ -0,0 +1,40 @@
+﻿using Harmic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Harmic.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly HarmicContext _context;
+
+        public BlogController(HarmicContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/blog/{alias}-{id}.html")]
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TblBlogs == null)
+            {
+                return NotFound();
+            }
+
+            var blog = await _context.TblBlogs
+                .FirstOrDefaultAsync(m => m.BlogId == id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+            ViewBag.blogComment = _context.TblBlogComments.Where(i => i.BlogId == id).ToList();
+            return View(blog);
+        }
+    }
+}
