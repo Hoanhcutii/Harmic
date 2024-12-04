9316cc11ec4a7b449cf297a7eca1578ac1f275d3
Hoanhcutii <hoaianh03082k4@gmail.com>
2024-12-04 21:34:55 +0700

    TH_10 nma da sua duoc anh hihi

---
+++ Harmic/Areas/Admin/Controllers/FileManagerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Harmic.Areas.Admin.Controllers
+{
+    public class FileManagerController : Controller
+    {
+        [Area("Admin")]
+        [Route("/Admin/file-manager")]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Harmic/Areas/Admin/Models/SummerNote.cs
@@ -0,0 +1,18 @@
+﻿namespace Harmic.Areas.Admin.Models
+{
+    public class SummerNote
+    {
+        public SummerNote(string idEditor, bool loadLibrary = true)
+        {
+            IDEditor = idEditor;
+            loadLibrary = loadLibrary;
+        }
+
+        public string IDEditor { get; set; }
+        public bool LoadLibrary { get; set; }
+        public int Height { set; get; } = 500;
+
+
+    }
+
+}
